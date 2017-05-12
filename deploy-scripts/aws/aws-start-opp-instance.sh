ea97e1fbc14bfb58c0da006f1a48a6e70ace053a
Craig Tobe <ctobe@constantcontact.com>
2017-05-12 11:38:15 -0400

    aws update

--- deploy-scripts/aws/aws-start-opp-instance.sh
+++ deploy-scripts/aws/aws-start-opp-instance.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+./aws-config.sh
+
 ###### Configs #######
 maxMinutesToWait=10
-PROFILE="--profile ctctperformance"
-INSTANCE_TYPE=t2.small
-ENV=development
-EBS_DELETE_ON_TERMINATION=true    # keep true for dev
-AMI=ami-c58c1dd3
-SECURITY_GROUP=sg-19e03267
 EC2_JSON_CONFIG_FILE=aws-opp-docker.json
 INIT_SCRIPT="--user-data file://aws-build-opp-docker-on-instance.sh"
 
